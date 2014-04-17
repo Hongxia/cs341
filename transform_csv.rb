f282a5f4eadd591dac6c1a113a5a624617df7a0f
James Liu <James@Jamess-MacBook-Air-3.local>
2014-04-16 17:44:04 -0700

    transform file added

---
+++ transform_csv.rb
@@ -0,0 +1,20 @@
+#Trim the files and write datapoints in csv
+datapoints = Array.new
+infile = File.new("Books.txt", "r")
+outfile = File.new("Books.csv", "w")
+datapoint = ""
+while (line = infile.gets)
+	if line.index('product/productId') != nil  then
+        datapoint = ""
+		datapoint = datapoint + "#{line[19..-2]},"
+    elsif line.index('review/userId') != nil then
+    	datapoint = datapoint + "#{line[15..-2]},"
+    elsif line.index('review/score') != nil then
+    	datapoint = datapoint + "#{line[14..-2]},"
+    elsif line.index('review/time') != nil then
+    	datapoint = datapoint + "#{line[13..-2]}\n"
+        outfile.write(datapoint)
+    end
+end
+infile.close
+outfile.close
