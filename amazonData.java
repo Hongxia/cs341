ac6bac7a31de780d37b115d610e6e9c18feb6763
Rahul Pandey <rpandey1234@gmail.com>
2014-04-22 01:39:01 -0700

    bucket by experience level

--- amazonData.java
+++ amazonData.java
@@ -3,6 +3,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map.Entry;
@@ -10,6 +11,8 @@ import java.util.Map.Entry;
 
 public class amazonData {
 
+	static int NUM_EXPERIENCE_LEVEL = 5;
+	
 	public static void main(String[] args) {
 		for (int i = 0; i < args.length; i++) {
 			System.out.println(args[i]);
@@ -24,8 +27,59 @@ public class amazonData {
 			System.exit(-1);
 		}
 		doStuff(ratingsMap);
+		HashMap<String, ArrayList<ArrayList<Rating>>> ratingsMapBucket = makeExpBuckets(ratingsMap); 
+		doStuffBucketed(ratingsMapBucket);
 	}
 	
+	private static void doStuffBucketed(
+			HashMap<String, ArrayList<ArrayList<Rating>>> ratingsMapBucket) {
+		SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm");
+		for (Entry<String, ArrayList<ArrayList<Rating>>> entry : ratingsMapBucket.entrySet()) {
+			ArrayList<ArrayList<Rating>> bucketedRatings = entry.getValue();
+			// Only print for users which have at least 3 ratings in exp level 5, which 
+			// means they have at least 15 ratings total
+			if (bucketedRatings.get(4).size() < 3) continue;
+			System.out.println("User: " + entry.getKey());
+			for (int i = 0; i < NUM_EXPERIENCE_LEVEL; i++) {
+				ArrayList<Rating> ratings = bucketedRatings.get(i);
+				if (ratings.size() == 0) {
+					System.out.println("Experience level " + (i + 1) + " has no reviews");
+				} else {
+					int numReviewsSoFar = ratings.size() * i;
+					System.out.println("Experience level " + (i + 1) + " after " + numReviewsSoFar + " reviews");
+					Rating r = ratings.get(0);
+					System.out.println("Starts at time: " + sdf.format(r.getTime()));
+				}				
+			}
+			System.out.println();
+		}
+		
+	}
+
+	private static HashMap<String, ArrayList<ArrayList<Rating>>> makeExpBuckets(
+			HashMap<String, ArrayList<Rating>> ratingsMap) {
+		HashMap<String, ArrayList<ArrayList<Rating>>> ratingsMapBucket = new HashMap<String, ArrayList<ArrayList<Rating>>>();
+		for (Entry<String, ArrayList<Rating> > entry : ratingsMap.entrySet()) {
+			ArrayList<Rating> ratings = entry.getValue();
+			Collections.sort(ratings);
+			// The integer index which represents 1/5 of the ratings of this user.
+			int indFraction = ratings.size() / NUM_EXPERIENCE_LEVEL;
+			
+			ArrayList<ArrayList<Rating>> bucketed = new ArrayList<ArrayList<Rating>>();
+			
+			for (int i = 0; i < NUM_EXPERIENCE_LEVEL; i++) {
+				ArrayList<Rating> expBucket = new ArrayList<Rating>(); 
+				for (int j = i*indFraction; j < (i+1)*indFraction ; j++) {
+					expBucket.add(ratings.get(j));
+				}
+				bucketed.add(expBucket);
+			}
+			
+			ratingsMapBucket.put(entry.getKey(), bucketed);
+		}
+		return ratingsMapBucket;
+	}
+
 	private static HashMap<String, ArrayList<Rating> > readFile(String filename) {
 		BufferedReader br = null;
 		try { 
@@ -90,7 +144,7 @@ public class amazonData {
 		System.out.println("Date of last rating: " + sdf.format(lastRating));
 	}
 	
-	static class Rating {
+	static class Rating implements Comparable<Rating> {
 		private String pid; // Product ID
 		private double rating; // User rating
 		private Date time; // Time of rating
@@ -117,6 +171,11 @@ public class amazonData {
 		}
 		public void setPid(String pid) {
 			this.pid = pid;
+		}
+		
+		@Override
+		public int compareTo(Rating r) {
+			return (this.time).compareTo(r.getTime());
 		}
 	}
 }
