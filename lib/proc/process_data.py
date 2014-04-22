d0839757f9e5f5e4cffce2fe87b82330f6d30735
Hongxia Zhong <hongxia.zhong@gmail.com>
2014-04-21 18:54:59 -0700

    adding threshold to script

--- lib/proc/process_data.py
+++ lib/proc/process_data.py
@@ -4,9 +4,15 @@ import sys
 import os
 from data import CSVDataProcessor
 
-if len(sys.argv) != 2:
+if len(sys.argv) < 2:
     print "usage: python process_data.py path_to_csv"
     exit(1)
+
+if len(sys.argv) == 3:
+    threshold = int(sys.argv[2])
+else:
+    threshold = 10
+print "threshold = %d" % threshold
 
 csv_files = []
 for filename in os.listdir(sys.argv[1]):
@@ -17,7 +23,7 @@ print "found %d csv files" % len(csv_files)
 for csv_file in csv_files:
     print "processing %s..." % csv_file
     csv_processor = CSVDataProcessor(os.path.join(sys.argv[1], csv_file))
-    csv_processor.read(threshold=0, rand_sampling=True)
-    csv_processor.read(threshold=0, rand_sampling=False)
+    csv_processor.read(threshold=threshold, rand_sampling=True)
+    csv_processor.read(threshold=threshold, rand_sampling=False)
 
 print "done!"
