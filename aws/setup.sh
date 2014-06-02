d42112ae1753f135b6255a5d482d5cce5e070468
Hongxia Zhong <hongxia.zhong@gmail.com>
2014-06-02 03:24:04 -0700

    adding setup script

---
+++ aws/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get install git-core
+sudo apt-get install python-pip python-dev build-essential
+sudo pip install numpy
+sudo apt-get install libatlas-base-dev gfortran
+sudo pip install scipy
