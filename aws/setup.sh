b85a4cee2113cac2386336d263dd76341d15bff9
Hongxia Zhong <hongxia.zhong@gmail.com>
2014-06-02 22:29:17 -0700

    adding s3cmd installation cmd

--- aws/setup.sh
+++ aws/setup.sh
@@ -4,5 +4,6 @@ sudo apt-get install python-pip python-dev build-essential
 sudo pip install numpy
 sudo apt-get install libatlas-base-dev gfortran
 sudo pip install scipy
+sudo apt-get install s3cmd
 
 git clone https://github.com/Hongxia/cs341.git
