e5e4dc07181f64368104eab3ff20c9698bb0b385
Hongxia Zhong <hongxia.zhong@gmail.com>
2014-06-08 16:45:03 -0700

    adding pulling data to setup

--- aws/setup.sh
+++ aws/setup.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-sudo apt-get install git-core
+
+# setup numpy and scipy
 sudo apt-get install python-pip python-dev build-essential
 sudo pip install numpy
 sudo apt-get install libatlas-base-dev gfortran
 sudo pip install scipy
-sudo apt-get install s3cmd
 
+# setup s3cmd
+sudo apt-get install s3cmd
+echo "ACCESS KEY: AKIAIGQCJ2QMLH6GQJGQ"
+echo "SECRET KEY: 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"
+s3cmd --configure
+
+# setup git repo
+sudo apt-get install git-core
 git clone https://github.com/Hongxia/cs341.git
+
+# pull data
+mkdir /home/ubuntu/cs341/data
+cd /home/ubuntu/cs341/data 
+get -r s3://cs341-amazon/hive-input/Arts_rand
+get -r s3://cs341-amazon/hive-input/Arts_temp
+get -r s3://cs341-amazon/hive-input/Books_rand
+get -r s3://cs341-amazon/hive-input/Books_temp
+get -r s3://cs341-amazon/hive-input/Gourmet_Foods_rand
+get -r s3://cs341-amazon/hive-input/Gourmet_Foods_temp
+get -r s3://cs341-amazon/hive-input/Movies_rand
+get -r s3://cs341-amazon/hive-input/Movies_temp
+get -r s3://cs341-amazon/hive-input/Music_rand
+get -r s3://cs341-amazon/hive-input/Music_temp
+get -r s3://cs341-amazon/hive-input/Video_Games_rand
+get -r s3://cs341-amazon/hive-input/Video_Games_temp
